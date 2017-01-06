c933387d521de7a3cd43f8e7dc154e818dedda5d
Akshay Narayan <akshay.narayan@berkeley.edu>
2017-01-06 07:24:27 +0000

    install zsh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# link config files
+ln -s ~/config/.bash_profile ~/.bash_profile
+ln -s ~/config/.gitconfig ~/.gitconfig
+ln -s ~/config/.gitignore ~/.gitignore
+ln -s ~/config/.tmux.conf ~/.tmux.conf
+ln -s ~/config/.zshrc ~/.zshrc
+
+# vim stuff
+mkdir -p ~/.vim
+cd ~/.vim
+git clone https://github.com/tpope/vim-pathogen.git
+cp -r vim-pathogen/autoload/ .
+
+# vim plugins
+mkdir -p bundle
+cd bundle
+git clone https://github.com/altercation/vim-colors-solarized.git
+git clone https://github.com/mileszs/ack.vim.git
+git clone https://github.com/airblade/vim-gitgutter.git
+git clone https://github.com/vim-airline/vim-airline.git
+git clone https://github.com/w0rp/ale.git
+git clone https://github.com/autozimu/LanguageClient-neovim.git
+
+cd ~/config
+ln -s ~/config/.vimrc ~/.vimrc
